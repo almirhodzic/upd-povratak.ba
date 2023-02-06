26021750c28662bcea0aa10921401e2bd51c5348
almirh <24458972+almirhodzic@users.noreply.github.com>
2023-02-06 12:46:57 +0100

    npm run build

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -32,7 +32,7 @@
 <!-- Content -->
 <div class="w-full lg:w-11/12 xl:w-10/12 2xl:w-9/12 pt-10 px-4 sm:px-6 md:px-8 md:pl-72 pb-10">
     <!-- Page Content -->@if (app()->isDownForMaintenance())
-        <div class="p-4 yellow-500 text-white flex items-center w-full rounded-md mb-6">Site is in maintenance mode!</div>
+        <div class="p-4 bg-yellow-500 text-white flex items-center w-full rounded-md mb-6">Site is in maintenance mode!</div>
     @endif
     @yield('content')
     <!-- End Page Content -->
