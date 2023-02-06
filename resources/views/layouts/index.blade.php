92902cc8d4db9c51c0ca37ce1b93985125d73c29
almirh <24458972+almirhodzic@users.noreply.github.com>
2023-02-06 12:52:51 +0100

    Update index.blade.php

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -19,9 +19,7 @@
 </head>
 <body class="bg-white dark:bg-slate-900">
 <!-- ========== MAIN CONTENT ========== -->
-@if (app()->isDownForMaintenance())
-    <div class="p-4 bg-pink-600 text-white flex items-center w-full rounded-md mb-6">Site is in maintenance mode!</div>
-@endif
+
 
 <!-- Sidebar Toggle -->
 <x-partials.sidebar-toggle />
@@ -34,6 +32,9 @@
 <!-- Content -->
 <div class="w-full lg:w-11/12 xl:w-10/12 2xl:w-9/12 pt-10 px-4 sm:px-6 md:px-8 md:pl-72 pb-10">
     <!-- Page Content -->
+    @if (app()->isDownForMaintenance())
+        <div class="p-4 bg-pink-600 text-white flex items-center w-full rounded-md mb-6">Site is in maintenance mode!</div>
+    @endif
     @yield('content')
     <!-- End Page Content -->
 </div>
