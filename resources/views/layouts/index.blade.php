a434ba0f9f4d128c83888dd7ae94595640531d6d
almirh <24458972+almirhodzic@users.noreply.github.com>
2023-02-11 10:47:57 +0100

    logo update

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -42,9 +42,9 @@
     @if (app()->isDownForMaintenance())
         <div class="p-4 bg-pink-600 text-white flex items-center w-full rounded-md mb-6">Site is in maintenance mode!</div>
     @endif
-    <div class="grid justify-items-center w-full mb-6 md:hidden">
+    <div class="grid justify-items-center w-full mb-2 md:hidden">
     <a href="{{ config('app.url') }}">
-        <img src="{{ asset('static/assets/img/logo-upd.povratak.ba.png') }}" class="object-scale-down h-36 w-36" alt="Logo {{ config('app.name') }} BiH">
+        <img src="{{ asset('static/assets/img/logo-upd.povratak.ba.png') }}" class="object-scale-down h-48 w-48" alt="Logo {{ config('app.name') }} BiH">
     </a>
     </div>
     @yield('content')
